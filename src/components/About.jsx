2cd84ab9c661b9fce7c790e8e3c2894146312b2b
Lay Cipriano <webdevlay@gmail.com>
2024-03-31 01:27:34 -0300

    alinhando informações de contato

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,18 +6,15 @@ export const About = () => {
             <div className="contentTwo">
                 <div className="sobreMim defaultSection">
                     <h2 className='h2'>Sobre Mim</h2>
-                    <p className='bottom'>Formada em Sistemas para Internet pela Unicesumar, onde iniciei minha carreira na área a pouco mais de 1 ano, me aventurando com a <span className='changePurple'>análise de requisitos</span>, definindo <span className='changePurple'>regras de negócio</span> e <span className='changePurple'>automatizando testes</span>.</p>
-                    <p className='top'>Paralelo à isso, desenvolvo minhas habilidades como frontend com alguns projetos apresentados aqui!</p>
+                    <p className='top'>Formada em Sistemas para Internet pela Unicesumar, onde iniciei minha carreira na área a pouco mais de 1 ano, me aventurando com a <span className='changePurple'>análise de requisitos</span>, definindo <span className='changePurple'>regras de negócio</span> e <span className='changePurple'>automatizando testes</span>.</p>
                 </div>
                 <div className="profissional defaultSection">
-                    <h2 className='h2'>Profissional</h2>
-                    <p className='bottom'>Após quase 2 anos de atuação em uma das franquias da Happy, na cidade de Maringá, sempre na área da gestão, comercial e administrativa, me foi dada a oportunidade de seguir na minha área de formação dentro do setor de TI da Sede da Happy Brasil, a Happy Tech. </p>
-                    <p className='top'>Aqui, junto minha experiência de negócio anterior para desenvolver novas soluções dentro do sistema próprio da empresa, aprimorando nosso ERP e CRM com as atualidades do mercado, bem como melhorando nossas ferramentas de treinamento. Vamos para quase 3 anos nessa parceria!</p>
+                    <p className='bottom'>Após quase 2 anos de atuação em uma das franquias da Happy, na cidade de Maringá, na área da gestão, comercial e administrativa, me foi dada a oportunidade de seguir na minha área de formação dentro do setor de TI da Sede da Happy Brasil, a Happy Tech. </p>
+                    <p className='top'>Aqui, faço uso da minha experiência de negócio anterior para desenvolver novas soluções dentro do sistema próprio da empresa, aprimorando nosso ERP e CRM com as atualidades do mercado!</p>
                 </div>
                 <div className="habilidades defaultSection">
-                    <h2 className='h2'>Habilidades</h2>
-                    <p className='bottom'>Minhas habilidades vão de uma excelente compreensão e capacidade de adaptação ao meu ambiente e pessoas, para um bom conhecimento das linguagens e frameworks utilizadas.</p>
-                    <p className='top'>Hoje fazemos uso de React para nosso frontend e Java Spring Boot para nosso backend e posso dizer que, junto da minha equipe atual, pude aprimorar muito meu conhecimento nessa área.</p>
+                    <p className='bottom'>Minhas habilidades vão de uma excelente compreensão e capacidade de adaptação ao meu ambiente e pessoas, para um bom conhecimento e entendimento das linguagens e frameworks utilizadas.</p>
+                    <p className='top'>Paralelo à isso, desenvolvo minhas habilidades como frontend com alguns projetos apresentados aqui!</p>
                 </div>
             </div>
         </>
